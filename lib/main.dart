212dbd741d642c0ff9bd23de9f111d6765c9117d
Maya <Glazerm22@gmail.com>
2023-11-02 16:41:48 +0200

    added the import for Cloud Firestore:

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:sqflite/sqflite.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:path/path.dart';
 
 void main() => runApp(MaterialApp(
